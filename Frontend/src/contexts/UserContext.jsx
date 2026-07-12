29aedd9bf4803c128e12f713579b39c54fa61d10
kb303 <kirstenbrown99@gmail.com>
2026-07-12 12:30:04 +1000

    frontend login functionality

--- Frontend/src/contexts/UserContext.jsx
+++ Frontend/src/contexts/UserContext.jsx
@@ -1,12 +1,15 @@
 import { createContext, useReducer, useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
+  const navigate = useNavigate();
+  const apiBaseUrl =
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+
   const createUser = async (userData) => {
-    const apiBaseUrl =
-      import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
     const url = `${apiBaseUrl}/api/user/register`;
     const { email, firstName, lastName, password } = userData;
 
@@ -17,10 +20,9 @@ export default function UserProvider({ children }) {
         lastName,
         password,
       });
-      console.log("Success:", response.data);
       console.log("Status Code:", response.status);
+      navigate("/");
     } catch (error) {
-      // Axios captures HTTP errors (like 400 or 500 codes) here
       if (error.response) {
         console.error("Server Error Data:", error.response.data);
         console.error("Server Status:", error.response.status);
@@ -30,8 +32,25 @@ export default function UserProvider({ children }) {
     }
   };
 
+  const loginUser = async (userData) => {
+    const url = `${apiBaseUrl}/api/user/login`;
+    const { email, password } = userData;
+
+    try {
+      const response = axios.post(url, { email, password });
+      console.log("Status Code:", response.status);
+      navigate("/");
+    } catch (error) {
+      if (error.response) {
+        console.error("Server Error Data:", error.response.data);
+        console.error("Server Status:", error.response.status);
+      } else {
+        console.error("Network Error:", error.message);
+      }
+    }
+  };
   return (
-    <UserContext.Provider value={{ createUser }}>
+    <UserContext.Provider value={{ createUser, loginUser }}>
       {children}
     </UserContext.Provider>
   );
